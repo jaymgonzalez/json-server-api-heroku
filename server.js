855ba626d780ed6c80d050c57c753f807b046716
Jaime Munoz <jmunoz@decibelinsight.com>
2020-12-10 12:29:36 +0100

    comment functions in server

--- server.js
+++ server.js
@@ -14,19 +14,16 @@ Relevant source code: https://github.com/typicode/json-server/blob/master/src/cl
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 // Can pass a limited number of options to this to override (some) defaults. See https://github.com/typicode/json-server#api
-const middlewares = jsonServer.defaults({
-  // Display json-server's built in homepage when json-server starts.
-  static: "node_modules/json-server/dist"
-});
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
 // To handle POST, PUT and PATCH you need to use a body-parser. Using JSON Server's bodyParser
-server.use(jsonServer.bodyParser);
+// server.use(jsonServer.bodyParser);
 
 // Simulate delay on all requests
 // server.use(function (req, res, next) {
@@ -36,22 +33,22 @@ server.use(jsonServer.bodyParser);
 // Declaring custom routes below. Add custom routes before JSON Server router
 
 // Add createdAt to all POSTS
-server.use((req, res) => {
-  if (req.method === "POST") {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
+// server.use((req, res) => {
+//   if (req.method === "POST") {
+//     req.body.createdAt = Date.now();
+//   }
+//   // Continue to JSON Server router
 
-});
+// });
 
-server.post("/courses/", function (req, res) {
-  const error = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error);
-  } else {
-    req.body.slug = createSlug(req.body.title); // Generate a slug for new courses.
-  }
-});
+// server.post("/courses/", function (req, res) {
+//   const error = validateCourse(req.body);
+//   if (error) {
+//     res.status(400).send(error);
+//   } else {
+//     req.body.slug = createSlug(req.body.title); // Generate a slug for new courses.
+//   }
+// });
 
 // Use default router
 server.use(router);
@@ -65,16 +62,16 @@ server.listen(port, () => {
 // Centralized logic
 
 // Returns a URL friendly slug
-function createSlug(value) {
-  return value
-    .replace(/[^a-z0-9_]+/gi, "-")
-    .replace(/^-|-$/g, "")
-    .toLowerCase();
-}
+// function createSlug(value) {
+//   return value
+//     .replace(/[^a-z0-9_]+/gi, "-")
+//     .replace(/^-|-$/g, "")
+//     .toLowerCase();
+// }
 
-function validateCourse(course) {
-  if (!course.title) return "Title is required.";
-  if (!course.authorId) return "Author is required.";
-  if (!course.category) return "Category is required.";
-  return "";
-}
+// function validateCourse(course) {
+//   if (!course.title) return "Title is required.";
+//   if (!course.authorId) return "Author is required.";
+//   if (!course.category) return "Category is required.";
+//   return "";
+// }
